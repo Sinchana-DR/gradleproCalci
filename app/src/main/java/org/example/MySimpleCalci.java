84aac7f1047741ed5a8af3da5cebc5a4ae2ec410
Sinchana-DR <sinchanadr2@gmail.com>
2025-06-09 18:46:26 +0530

    Initial commit

---
+++ app/src/main/java/org/example/MySimpleCalci.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class MySimpleCalci {
+	int addition(int a, int b) {
+		return a + b;
+	}
+	
+	int subtraction(int a, int b) {
+		return a - b;
+	}
+	
+	int multiplication(int a, int b) {
+		return a * b;
+	}
+	
+	float division(int a, int b) {
+		return (float) (a) / b;
+	}
+	
+	int remainder(int a, int b) {
+		return a % b;
+	}
+
+}
